aa6bac4da0a2d0fa253bf1c43a89fcc1dec6a647
chew01 <chewjh19@gmail.com>
2022-04-09 01:49:00 +0800

    Project dependency and configs

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        fontFamily: {
+            'title': ['Baskerville'],
+            'menu': ['FiraSans'],
+        },
+        colors: {
+            'prime-100': '#F7F6F2',
+            'prime-200': '#F0E5CF',
+            'prime-300': '#C8C6C6',
+            'prime-400': '#4B6587',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
