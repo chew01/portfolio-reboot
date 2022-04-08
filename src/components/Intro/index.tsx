02247b9bda3ccab6f92c85e10895a8456c61e737
chew01 <chewjh19@gmail.com>
2022-04-09 01:51:16 +0800

    Boilerplate components to be added (WIP)

---
+++ src/components/Intro/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Intro() {
+  return <section id="intro">Intro</section>;
+}
+
+export default Intro;
---
+++ src/components/Main/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Intro from '../Intro';
+import About from '../About';
+import Projects from '../Projects';
+
+function Main() {
+  return (
+    <main>
+      <Intro />
+      <About />
+      <Projects />
+    </main>
+  );
+}
+
+export default Main;
